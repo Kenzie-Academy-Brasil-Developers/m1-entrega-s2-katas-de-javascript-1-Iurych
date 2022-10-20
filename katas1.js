1fffc65d75f9fd1139dad09b91ff7a56c778c4fa
iurych <iury.vibe@gmail.com>
2022-10-20 16:04:27 -0300

    kata

--- katas1.js
+++ katas1.js
@@ -1,1 +1,31 @@
+//exercicio1
+/*
+function cubicSquare () {
+    let resultado = ''
+    for (let i=1 ; i<=10 ; i++) {
+    let a = i ** 3
+    resultado += `${a}, ` //ageitar a última ,
+  
+}
+return resultado
+}
+console.log(cubicSquare())
+*/
+
+//exercicio2
+
+function divisiblesFor(x , y){
+    let divisores = '';
+    for (let i = x ; i >= y ; i--) {
+        //console.log(i);
+        let q =parseInt(i / y);
+        //console.log(q);
+        divisores += `${q}, `;
+
+    }
+return divisores
+}
+console.log('Divisores: ' + divisiblesFor(100,10))
+
+
 
