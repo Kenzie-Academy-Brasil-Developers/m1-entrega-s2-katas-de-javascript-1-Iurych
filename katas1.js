d691311b5f9e75e627049fff63c19cbb39828572
iurych <iury.vibe@gmail.com>
2022-10-22 11:18:43 -0300

    exe. 1 e 2

--- katas1.js
+++ katas1.js
@@ -1,31 +1,50 @@
 //exercicio1
-/*
+
 function cubicSquare () {
     let resultado = ''
     for (let i=1 ; i<=10 ; i++) {
     let a = i ** 3
-    resultado += `${a}, ` //ageitar a última ,
+    resultado += `${a} ` //ageitar a última ,
   
 }
 return resultado
 }
+console.log("exercicio1")
 console.log(cubicSquare())
+
+/*
+//exercicio2
+function divisiblesFor(x, y){
+    let Divisores = ''
+    for (let i = 1 ; i <= x ; i++) {
+        if (i % y == 0) {
+            let Q = i 
+            Divisores += `${Q} `
+
+        }
+    }return Divisores
+
+}
+console.log("exercicio2")
+console.log(divisiblesFor(20 , 2))//verificar o undefined no final
 */
 
-//exercicio2
+/*
+//exercício3
+//1. String, a palavra a ser utilizada
+//2. Number, a posição a ser procurada
 
-function divisiblesFor(x , y){
-    let divisores = '';
-    for (let i = x ; i >= y ; i--) {
-        //console.log(i);
-        let q =parseInt(i / y);
-        //console.log(q);
-        divisores += `${q}, `;
+function stringElement(x, y){
+    let q = []
+    for (let i = 1 ; i < x.length ; i++ ) {
+        console.log(i)
+        if (i % y == 0) {
+            q.push( i / y) 
+        }
 
     }
-return divisores
+    return q.join('/')
 }
-console.log('Divisores: ' + divisiblesFor(100,10))
-
-
-
+console.log("exercicio3")
+console.log (stringElement("iury", 3 ))
+*/
