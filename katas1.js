e00dfdc6e3ddd63b1181c637b2cc1248e2b232c1
iurych <iury.vibe@gmail.com>
2022-10-23 10:53:12 -0300

    exercicios 1 - 4

--- katas1.js
+++ katas1.js
@@ -9,11 +9,11 @@ function cubicSquare () {
 }
 return resultado
 }
-console.log("exercicio1")
+console.log("exercicio 1")
 console.log(cubicSquare())
-*/
 
-/*
+
+
 //exercicio2
 function divisiblesFor(x, y){
     let Divisores = ''
@@ -26,17 +26,18 @@ function divisiblesFor(x, y){
     }return Divisores
 
 }
-console.log("exercicio2")
+console.log("exercicio 2")
 console.log(divisiblesFor(20 , 2))//verificar o undefined no final
-*/
 
-/*
+
+
 //exercício3
 function stringElement(string, n){
     
     //começar do index 0 e percorrer até o últimp 
-    for (let i = 0 ; i <= string.length-1 ; i++ ) {
-        if (n == i){
+    for (let i = 0 ; i <= string.length ; i++ ) {
+        if (i == n){
+            return string[i].toUpperCase()
             console.log(`Retorna ${string[i].toUpperCase()} pois essa é a letra da posição ${n}`)
 
         }
@@ -46,11 +47,25 @@ function stringElement(string, n){
 
 }
 console.log("exerciocio 3")
-console.log(stringElement("iury", 0))//verificar o undefined
+let resultado = stringElement("iury", 0)
+console.log(resultado)
+//console.log(stringElement("iury", 3))//verificar o undefined
 */
 
+//exercicio4
+function stringSlicer(string, n) {
+    for(let i = 0 ; i <= string.length ; i++) {
+         
+        if (n) {
+            return string.slice(0, n) // porque se coloco console.log da undefined?
 
+        }
+     }
 
+}
+console.log("exercicio 4")
+let resultado4 = stringSlicer("almoxarifado", 4)
+console.log(`Retorna ${resultado4.toUpperCase()} pois são os 4 primeiros caracteres da string.`)
 
 
 
